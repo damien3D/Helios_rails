9b464ca34736ab3f23c94d846b63c0ce9f5ce735
rdaneel <dan.raynal@gmail.com>
2014-10-10 17:14:18 +0200

    Initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeliosRails::Application.config.secret_key_base = '2f57de2673bdbe32c08e8b4541680d857cc6dc307e30dd589dd71150ac53823acb7bccd100f4cf1077333c0c337b0e4b2399c297907556ad674cbbcffcdfffaa'
