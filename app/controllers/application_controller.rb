a8ee0c5b437fa3906fea16178f600aea958be1bf
rdaneel <dan.raynal@gmail.com>
2014-10-11 16:20:31 +0200

    Fourth again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
 # For APIs, you may want to use :null_session instead.
  
 protect_from_forgery with: :exception
+
+  def home 
+
+  end
  
 before_action :setup_mcapi
  
@@ -14,9 +18,7 @@ before_action :setup_mcapi
     @list_id = "964a273b37"
   end
 
-  def home 
 
-  end
 
 
 end
