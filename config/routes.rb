ea3ec386511b38b004e2fe464be4b26b212e04b6
rdaneel <dan.raynal@gmail.com>
2014-10-11 20:21:09 +0200

    Sixth attempt deleting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 HeliosRails::Application.routes.draw do
-  root 'application#home'
+  get '', to: 'application#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
