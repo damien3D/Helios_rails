1a154010d7814550a4c30c6596ea314ee517886f
rdaneel <dan.raynal@gmail.com>
2014-10-11 20:23:20 +0200

    "Seventh delete root"

--- app/mailers/your_mailer.rb
+++
@@ -1,7 +0,0 @@
-class YourMailer < ActionMailer::Base
-  def email_name
-    mail :subject => "Mandrill rides the Rails!",
-         :to      => "recipient@example.com",
-         :from    => "you@yourdomain.com"
-  end
-end
